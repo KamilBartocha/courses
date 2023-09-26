8fa126d661cff2c330eb87c1e25f47f6e9ccdff8
Kamil Bartocha <kamilbartocha53@gmail.com>
2023-09-26 15:52:24 +0200

    Add selenium tests

---
+++ test_automation/003_selenium_automation/test_google_search.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import *
+
+driver = webdriver.Safari()
+driver.get("https://google.com")
+print(driver.title)
+driver.implicitly_wait(3)
+accept_all = driver.find_element(By.ID, "L2AGLb")
+accept_all.click()
+
+driver.implicitly_wait(3)
+search = driver.find_element(By.ID, "APjFqb")
+search.send_keys("selenium test")
+
+driver.implicitly_wait(3)
+search.send_keys(Keys.RETURN)
+
+sleep(4)
+
+results = driver.find_element(By.ID, "result-stats")
+print(results.text)
+
+assert results.text is not None
+driver.quit()
