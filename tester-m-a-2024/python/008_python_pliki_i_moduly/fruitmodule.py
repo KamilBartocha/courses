d919b65abe3aa54a0bcdb3875eae79d5681e0f43
Kamil Bartocha <kamilbartocha53@gmail.com>
2024-10-01 18:00:51 +0200

    add

---
+++ tester-m-a-2024/python/008_python_pliki_i_moduly/fruitmodule.py
@@ -0,0 +1,2 @@
+def lemon_number(l):
+    print('Lemoniada nr', l)
---
+++ tester-m-a-2024/python/cw2.py
@@ -0,0 +1,11 @@
+# s = 'First line.\n\tSecond line.'  # \n oznacza znak nowej linii
+# print(s)
+# x = 5
+# print(x)
+
+lata = input("lata: ")
+oprocentowanie = input("Wprowadź oprocentowanie: ")
+
+print(lata)
+print(oprocentowanie)
+print(6 * lata)# "6" * "7" - nie działa
