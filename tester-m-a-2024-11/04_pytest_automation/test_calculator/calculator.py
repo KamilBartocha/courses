cfedd8a8ba4166bc9ec3aba728df26c4f39bdfc5
Kamil Bartocha <kamilbartocha53@gmail.com>
2024-11-26 17:22:59 +0100

    clean up order

---
+++ tester-m-a-2024-11/04_pytest_automation/test_calculator/calculator.py
@@ -0,0 +1,7 @@
+class Calculator:
+    def __add__(a, b):
+        return a + b
+
+    def subtract(a, b):
+        """Returns result of a - b"""
+        return a - b
---
+++ tester-m-a-2024-11/05_selenium_automation/test_website_title.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+def get_website_title(url):
+    driver = webdriver.Safari()
+    driver.get(url)
+    title = driver.title
+    driver.quit()
+    return title
+
+def test_codebrainers_title():
+    url = "https://codebrainers.pl/"
+    expected_title = "Coś więcej niż kodowanie - codebrainers"
+    website_title = get_website_title(url)
+    assert expected_title == website_title
+
+def test_google_title():
+    url = "https://google.com/"
+    expected_title = "Google"
+    website_title = get_website_title(url)
+    assert expected_title == website_title
