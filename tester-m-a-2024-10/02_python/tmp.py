3c822ef96810538ee287c92e4d82b70e66f0a5bf
Kamil Bartocha <kamilbartocha53@gmail.com>
2024-11-30 15:41:29 +0100

    add after saturday

---
+++ tester-m-a-2024-10/02_python/tmp.py
@@ -0,0 +1,26 @@
+counter = 4
+tekst = "Python "
+print(tekst * counter)
+print("-" * 60)
+print(tekst * counter)
+
+
+x = "Ala ma kota 'Burek'"
+y = "Ala ma kota \"Burek\""
+
+print(x)
+print(y)
+
+print("Ala \nma kota")
+print("\tAla ma kota")
+
+print(r"C:\Users\natalia")    # to mój kod print(sadasda)
+
+"""mój kod
+jest  print(alsdladasda)
+super!"""
+
+
+'''mój kod
+jest  print(alsdladasda)
+super!'''
