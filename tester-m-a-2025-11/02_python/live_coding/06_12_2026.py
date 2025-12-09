e906be9942f40500bbea38a10a736506e22a9858
Kamil Bartocha <kamilbartocha53@gmail.com>
2025-12-09 17:57:04 +0100

    update after first classes

--- tester-m-a-2025-11/02_python/live_coding/06_12_2026.py
+++ tester-m-a-2025-11/02_python/live_coding/06_12_2026.py
@@ -1,1 +1,64 @@
 print(3)
+print(3)
+
+number = 11
+
+print("nu#^$^%$^234234234dfdsfsdfdsfmber')")
+print(number)
+print("number")
+print('Ala ma kota')
+print(3)
+print(number)
+
+print('Ala ma kotaaaaaaaaaaaa\nAla ma kota 2')
+print('Ala ma kotaaaaaaaaaaaa\n\t\t\tAla ma kota 2')
+
+# Ćwiczenie: wyświetl F
+
+text = "ala ma kota Filemona"
+
+print(text[12]) #dodatni
+
+print(text[-8]) #ujemny
+
+# zmienna = input()
+# print(zmienna)
+
+# temperatura = input("Podaj temperaturę: ")
+
+# lata = input()
+# print("wpisałeś wartość: " + lata)
+
+# oprocentowanie = input("Wprowadź oprocentowanie: ")
+# print("wpisałeś wartość: " + oprocentowanie)
+
+
+
+
+
+x = []
+print(x)
+print(type(x))
+
+x = [1, 2, 3, 4, 5]
+print(x)
+
+print(x[3])
+print(x[3:])
+
+
+my_list = ["x", "y", "z", 2]
+print(my_list[2])
+my_list.append("r")
+print(my_list)
+print(my_list[1:2])
+print(len(my_list))
+
+
+
+
+list1 = ["a", "b"]
+list2 = [2, 3]
+
+list3 = [list1, list2, list1]
+print(list3) #[['a', 'b'], [2, 3], ['a', 'b']]
